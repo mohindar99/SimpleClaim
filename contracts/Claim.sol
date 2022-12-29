48c920504c8407334045a8e780a4b3b998713bf7
mohindar99 <d.mohindar1999@gmail.com>
2022-12-30 00:49:41 +0530

    second commit

--- contracts/Claim.sol
+++ contracts/Claim.sol
@@ -6,40 +6,41 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Claim is Ownable {
-   using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20;
 
-   mapping(address => bool) private moderator;
-   mapping(address => uint256) private balance;
-   IERC20 private claimToken;
+    mapping(address => bool) private moderator;
+    mapping(address => uint256) private balance;
+    IERC20 private claimToken;
 
-   constructor (
-      address[] memory moderators_,
-      uint256[] memory balances_,
-      IERC20 claimToken_
-   ) {
-      for (uint8 i = 0; i < 5; i ++) {
-         moderator[moderators_[i]] = true;
-         balance[moderators_[i]] = balances_[i];
-      }
+    constructor(
+        address[] memory moderators_,
+        uint256[] memory balances_,
+        IERC20 claimToken_
+    ) {
+        assert(moderators_.length == balances_.length);
+        for (uint8 i = 0; i < moderators_.length; i++) {
+            moderator[moderators_[i]] = true;
+            balance[moderators_[i]] = balances_[i];
+        }
 
-      claimToken = claimToken_;
-   }
+        claimToken = claimToken_;
+    }
 
-   modifier onlyModerator() {
-      require (moderator[msg.sender] == true, 'no permission');
-      _;
-   }
+    modifier onlyModerator() {
+        require(moderator[msg.sender] == true, "no permission");
+        _;
+    }
 
-   function claim() external onlyModerator {
-      address sender = msg.sender;
-      uint256 bal = balance[sender];
-      require (bal > 0, 'no balance');
+    function claim() external onlyModerator {
+        address sender = msg.sender;
+        uint256 bal = balance[sender];
+        require(bal > 0, "no balance");
 
-      balance[sender] = 0;
-      claimToken.safeTransfer(sender, bal);
-   }
+        balance[sender] = 0;
+        claimToken.safeTransfer(sender, bal);
+    }
 
-   function tokenBalance(address account) external view returns (uint256) {
-      return balance[account];
-   }
+    function tokenBalance(address account) external view returns (uint256) {
+        return balance[account];
+    }
 }
